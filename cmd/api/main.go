a9b749653befa002711aa90ecd3133c57bf1ec52
Julio Perdiguer <julio.perdiguer@zolar.de>
2024-09-24 11:42:02 +0200

    fixed main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"golang-template/cmd/api/bootstrap"
 	_ "golang-template/docs"
-	"golang-template/internal/platform/database/mysql"
 	"log"
 )
 
 func main() {
 
-	mysql.ConnectDB()
 	if err := bootstrap.Run(); err != nil {
 		log.Fatal(err)
 	}
